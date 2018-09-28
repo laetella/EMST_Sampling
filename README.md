404bdba96257b2eb85924d32898c1e811a877a66
laetella <laetella@outlook.com>
2018-09-28 15:42:48 +0800

    propose question

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # EMST_Sampling
 improving the efficiency of emst algorithm with sampling
+
+# 9.28
+思路： 结合之前做的EMST的算法，用采样的方法提高效率
+
+ 用 小得多的样本表示大数据集
+
+ 分组， 或者分层
--- code/our_method.py
+++ code/our_method.py
@@ -13,4 +13,7 @@ from headers import *
 if __name__ == '__main__':  
     fileName = "../data/chaining.dat"   # 8
     point_set = loadData(fileName, float, " ")
-    plt_point(point_set,fileName)
+    # plt_point(point_set,fileName)
+    # 进行采样（如何采样）
+    # 对采样的数据创建EMST,然后剩下的数据怎么办?---近似的,然后再得到精确的
+    # 如何获得精确的EMST
