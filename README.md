a184374dc9a5d7e7345ab7d14508a8ce360fd955
laetella <laetella@outlook.com>
2018-09-30 16:18:38 +0800

    some idea

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ improving the efficiency of emst algorithm with sampling
  用 小得多的样本表示大数据集
 
  分组， 或者分层
+ 思路： 缩小数据集，
+ 1. 对所有数据，计算相似性， 相似的点删除，
+ 2. 对于剩下的点（即采样得到的点）计算MST，并进行聚类
+ 3. 把相似的点加进聚类结果中
+ 可以采用  相似的点要进行标记
--- utils/file_operation.py
+++ utils/file_operation.py
@@ -10,6 +10,5 @@ def loadData(fileName,data_type, str):
     point_set = [] 
     for line in open(fileName, 'r'): 
         point = [data_type(data) for data in line.split(str)]
-        # point_set.append([point[1],point[2]])
         point_set.append(point)
     return point_set
